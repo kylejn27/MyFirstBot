097841c5b181dc4cb1d41802d48559efd88bd867
Kyle Nicholson <kylejn27@gatech.edu>
2021-12-07 10:08:30 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MyFirstBot {
+    class Program {
+        static void Main(string[] args) {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync() {
+            Console.WriteLine("Async Main!");
+        }
+    }
+}
